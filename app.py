9308fed7bdfd15697a7a1e0d2c662631c5371d5b
zaynab99 <91498010+zaynab99@users.noreply.github.com>
2021-09-28 01:01:19 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+miles = input('Enter a distance in miles: ')
+# kilometers_value = miles_value * 1.609344
+
+#kilometers
+miles_float = float(miles) 
+kilometres = miles_float * 1.609344
+
+print("distance in kilometers: ", kilometres)
---
+++ for_loop_challenge.py
@@ -0,0 +1,15 @@
+fruits = [
+    'apples',
+    'bananas',
+    'dragon fruit',
+    'mangos',
+    'nectarines',
+    'pears',
+]
+
+# for loop challenge
+
+print("our fruit selection")
+
+for fruit in fruits:
+    print(fruit)
